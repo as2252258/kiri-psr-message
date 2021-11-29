3b16f6855356f07a692770a184c412072824aab1
向林 <as2252258@163.com>
2021-11-29 16:30:03 +0800

    改名

---
+++ src/Constrict/Validator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Http\Constrict;
+
+class Validator
+{
+
+
+	private bool $isSuccess;
+	private string $message;
+
+
+	/**
+	 * @param bool $isSuccess
+	 * @param string $message
+	 */
+	public function __construct(bool $isSuccess, string $message)
+	{
+		$this->isSuccess = $isSuccess;
+		$this->message = $message;
+	}
+
+
+	/**
+	 * @return bool
+	 */
+	public function success(): bool
+	{
+		return $this->isSuccess;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function error(): string
+	{
+		return $this->message;
+	}
+
+}
